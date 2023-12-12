498520132b644f6b1bbb8ce79ddf7f49507e469d
weironrx <dimansaper1212@gmail.com>
2023-12-12 15:56:03 +0300

    spoiler

---
+++ spoiler (accordion)/jquery-script.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+    $(".block__title").click(function() {
+        if ($(this).parent().parent().hasClass("one")) {
+            $(".block__title").not($(this)).removeClass("active")
+            $(".block__title").not($(this)).next().slideUp(300)
+        }
+        $(this).toggleClass('active').next().slideToggle(300)
+    })
+});
