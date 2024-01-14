562ffdfaa397bbc0ab9ac535f84c3a76145dcfc7
dmitry <dmitri.p.sft@gmail.com>
2024-01-14 18:44:01 +0300

    important project popup changes p-final

--- logotype/js/script.js
+++ logotype/js/script.js
@@ -1,14 +1,21 @@
 const iconMenu = document.querySelector(".menu__icon")
 const background = document.querySelector(".background")
+const menuBody = document.querySelector(".menu")
 
-if (iconMenu) {
-    const menuBody = document.querySelector(".menu")
-    
-    iconMenu.addEventListener("click", () => {
+const action = () => {
     document.body.classList.toggle("_locked")
 
-        iconMenu.classList.toggle("_active")
-        menuBody.classList.toggle("_active")
-        background.classList.toggle("_active")
-    })
+    iconMenu.classList.toggle("_active")
+    menuBody.classList.toggle("_active")
+    background.classList.toggle("_active")
+}
+
+
+if (iconMenu) {
+    
+    iconMenu.addEventListener("click", () => action())
+
+    menuBody.addEventListener("click", (e) => e.stopPropagation())
+
+    background.addEventListener("click", () => action())
 }
